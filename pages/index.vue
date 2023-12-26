3f7138e6b7b885d3dab1669f1b21fb07f080093b
KBKUN024 <944412796@qq.com>
2023-12-26 19:25:33 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>index</div>
+</template>
+
+<script>
+// import pkg from '~/static/ethereumprovider'
+export default {
+  mounted() {
+    // console.log(pkg)
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ static/ethereumprovider.js
@@ -0,0 +1,2 @@
+import pkg from "https://esm.run/@walletconnect/ethereum-provider@2.9.0/dist/index.umd.js"
+export default pkg
